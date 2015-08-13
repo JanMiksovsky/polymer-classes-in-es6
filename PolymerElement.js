ae9c85c84c8b6cd1ffb12e81c93dc897c9170121
Jan Miksovsky <jan@quickui.org>
2015-08-13 16:04:01 -0700

    Upgrade to Polymer 1.x. Fix for Polymer 1.x constructor shenanigans.

--- PolymerElement.js
+++ PolymerElement.js
@@ -1,5 +1,7 @@
 /* Base class for defining custom elements. */
-class PolymerElement extends Polymer.Class({}) {
+let PolymerElementBase = Polymer.Class({ is: 'polymer-element' });
+
+class PolymerElement extends PolymerElementBase {
 
   /*
    * The default tag name for element converts Pascal-case class name FooElement
@@ -7,8 +9,8 @@ class PolymerElement extends Polymer.Class({}) {
    * a class, override this is() getter to just return the name as a hard-coded
    * string ("my-element" or whatever).
    */
-  get is() {
-    let className = this.constructor.name;
+  static get defaultElementName() {
+    let className = this.name;
     let upperCaseWordsRegex = /([A-Z][^A-Z]+)/g;
     let words = [];
     let match = upperCaseWordsRegex.exec( className );
@@ -20,6 +22,13 @@ class PolymerElement extends Polymer.Class({}) {
   }
 
   static register() {
+    if (this.prototype.is == null || this.prototype.is === 'polymer-element') {
+      // The following call to registerCallback will invoke some constructor
+      // shenanigans. Before that happens, we use the real constructor to
+      // compute a default element name.
+      this.prototype.is = this.defaultElementName;
+    }
+
     // Try to force the usual Polymer registration to happen. This is a pure
     // hack, with no guarantees that this does everything the way Polymer
     // normally would. At the moment (8/13/15), however, it does appear to work.
